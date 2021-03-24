f8a4bc702b3252a4f0646ba1731ea311d73cea3b
StephNaum <you@example.com>
2021-03-24 12:49:41 -0500

    sorting completed

--- script.js
+++ script.js
@@ -3,46 +3,48 @@ window.addEventListener("load", function(){
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
         response.json().then(function(json){
             let astronautCount = 0;
+            let spaceTime = [];
             for (let i=0; i<json.length; i++){
-                let newDiv = document.createElement('div');
-                newDiv.innerHTML =`<div class="astronaut">
-                        <div class="bio">
-                        <h3>${json[i].firstName} ${json[i].lastName}</h3>
-                        <ul>
-                            <li>Hours in space: ${json[i].hoursInSpace}</li>
-                            <li>Active: ${json[i].active}</li>
-                            <li>Skills: ${json[i].skills}</li>
-                        </ul>
-                        </div>
-                        <img class="avatar" src="${json[i].picture}">
-                    </div>`;
-                document.body.appendChild(newDiv);
-                // Index is necessary to access the 2nd <li>
-                let newClass = newDiv.getElementsByTagName("li")[1].setAttribute("class", "active");
-                let active = newDiv.getElementsByClassName("active");
-                // This changes the Active list if true to green
-                if (json[i].active){
-                    // This for loop is required b/c getElementsByClassName creates an array, as such we can't access properties without looping through it.
-                    for(let j = 0; j < active.length; j++) {
-                        active[j].style.color = "green";
-                      } 
-                } 
-                // Incrementing astronautCount using loop
-                astronautCount = i + 1;
-                
-            
-                    
-                    
-            }
+                // Sorting astronauts by most to least time in space
+                spaceTime.push(json[i].hoursInSpace);
+                spaceTime.sort(function(a,b){return b-a});
+            }; 
+            for (let l=0; l<json.length; l++) {
+                for (let k=0; k<json.length;k++){
+                    if (spaceTime[l] === json[k].hoursInSpace){
+                        let newDiv = document.createElement('div');
+                        newDiv.innerHTML =`<div class="astronaut">
+                            <div class="bio">
+                            <h3>${json[k].firstName} ${json[k].lastName}</h3>
+                            <ul>
+                                <li>Hours in space: ${json[k].hoursInSpace}</li>
+                                <li>Active: ${json[k].active}</li>
+                                <li>Skills: ${json[k].skills}</li>
+                            </ul>
+                            </div>
+                            <img class="avatar" src="${json[k].picture}">
+                            </div>`;
+                        document.body.appendChild(newDiv);
+                        // Index is necessary to access the 2nd <li>
+                        let newClass = newDiv.getElementsByTagName("li")[1].setAttribute("class", "active");
+                        let active = newDiv.getElementsByClassName("active");
+                        // This changes list if Active is true to green
+                        if (json[k].active){
+                            // This for loop is required b/c getElementsByClassName creates an array, as such we can't access properties without looping through it.
+                            for(let j = 0; j < active.length; j++) {
+                                active[j].style.color = "green";
+                            } 
+                        }; 
+                        // Incrementing astronautCount using loop
+                        astronautCount = k + 1;
+                    }
+                }
+            }     
             // Adding count of astronauts to page
             let phrase = `We have ${astronautCount} astronauts.`;
             let h = document.createElement("h2");
             h.innerHTML = phrase;
             document.body.appendChild(h);
-            
         });
     });
-
-
-
 });
